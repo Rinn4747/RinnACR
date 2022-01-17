ae8078583a380fd606f7c2195a376420a1e106ed
Rinn4747 <86368454+Rinn4747@users.noreply.github.com>
2022-01-17 07:54:07 +0100

    now include aoe mudra, and some distance check, should now work-ish even at lower levels

--- 80 - PVE - NIN - Rinn.lua
+++ 80 - PVE - NIN - Rinn.lua
@@ -34,7 +34,8 @@ profile.ninjaSkill =
 		jin2 = {18807,false},
 		suiton = {2271,true},
 		doton = {2270,false},
-		suiton = {2271,true},
+		huton = {2269,false},
+		katon = {2266,true},
 		bhavacakra = {7402,true},
 		hellfrogmedium = {7401,true},
 		mug = {2248,true},
@@ -44,17 +45,18 @@ profile.ninjaSkill =
 		trickattack = {2258,true},
 		raiton = {2267,true},
 		kassatsu = {2264,false},
+		katon2 = {16491,true},
 		hyoton2 = {16492,true},
 		tenshijin = {7403,false},
-		tsj1 = {18873,true},
-		tsj2 = {18874,true},
-		tsj3 = {18875,true},
-		tsj4 = {18876,true},
-		tsj5 = {18877,true},
-		tsj6 = {18878,true},
-		tsj7 = {18879,true},
-		tsj8 = {18880,true},
-		tsj9 = {18881,true},
+		tsj1 = {18873,true}, --fuma 
+		tsj2 = {18874,true}, --fuma 
+		tsj3 = {18875,true}, --fuma 
+		tsj4 = {18876,true}, --katon
+		tsj5 = {18877,true}, --raiton
+		tsj6 = {18878,true}, --hyoton
+		tsj7 = {18879,true}, --huton
+		tsj8 = {18880,false}, --doton
+		tsj9 = {18881,true}, --suiton
 	}	
 
 function profile:skillID(string)
@@ -166,15 +168,17 @@ profile.mudra = {}
 profile.mudraresult = ""
 profile.mudracounter = 0
 profile.mudranumber = 0
-profile.ntsj1 = 0
-profile.ntsj2 = 0
-profile.ntsj3 = 0
-profile.ntsj4 = 0
-profile.ntsj5 = 0
-profile.ntsj6 = 0
-profile.ntsj7 = 0
-profile.ntsj8 = 0
-profile.ntsj9 = 0
+profile.mudratarget = ""
+profile.ntsj1 = false
+profile.ntsj2 = false
+profile.ntsj3 = false
+profile.ntsj4 = false
+profile.ntsj5 = false
+profile.ntsj6 = false
+profile.ntsj7 = false
+profile.ntsj8 = false
+profile.ntsj9 = false
+profile.ninjutsumaxtimer = 0
 
 function profile.Cast()
     local currentTarget = MGetTarget()
@@ -183,94 +187,170 @@ function profile.Cast()
 		if Player:IsMoving() then
 			profile.safejump = Now()
 		end
-		--d(profile.castingNinjutsu)
+		if TimeSince(profile.ninjutsumaxtimer) > 5000 and profile.castingNinjutsu  then
+			profile.castingNinjutsu = false
+		end
+		--d("casting ninjutsu : "..tostring(profile.castingNinjutsu))
+		--d("ninjutsu result : "..tostring(profile.mudraresult))
 		--tenshijin
-		if profile.checkEach({"tenshijin"},false) and not profile:hasBuffSelf("tenshijin") then	
+		if profile.checkEach({"tenshijin"},false) and not profile:hasBuffSelf("tenshijin") and not profile.castingNinjutsu then
+			profile.ntsj1 = true
+			profile.ntsj2 = true
+			profile.ntsj3 = true
+			profile.ntsj4 = true
+			profile.ntsj5 = true
+			profile.ntsj6 = true
+			profile.ntsj7 = true
+			profile.ntsj8 = true
+			profile.ntsj9 = true		
 			return true
 		end
-		if profile:hasBuffSelf("tenshijin") and TimeSince(profile.ntsj1) > 10000 and profile.checkEach({"tsj1"}) then
-			profile.ntsj1 = Now()
-			d("tsj1")
-			return true
-		end		
-		if profile:hasBuffSelf("tenshijin") and TimeSince(profile.ntsj9) > 10000 and profile.checkEach({"tsj9"})  then
-			profile.ntsj9 = Now()
-			d("tsj9")
-			return true
-		end
-		if profile:hasBuffSelf("tenshijin") and TimeSince(profile.ntsj8) > 10000 and profile.checkEach({"tsj8"}) then
-			profile.ntsj8 = Now()
-			d("tsj8")
-			return true
-		end
-		if profile:hasBuffSelf("tenshijin") and TimeSince(profile.ntsj7) > 10000 and profile.checkEach({"tsj7"}) then
-			profile.ntsj7 = Now()
-			d("tsj7")
-			return true
-		end
-		if profile:hasBuffSelf("tenshijin") and TimeSince(profile.ntsj6) > 10000 and profile.checkEach({"tsj6"}) then
-			profile.ntsj6 = Now()
-			d("tsj6")
-			return true
-		end
-		if profile:hasBuffSelf("tenshijin") and TimeSince(profile.ntsj5) > 10000 and profile.checkEach({"tsj5"}) then
-			profile.ntsj5 = Now()
-			d("tsj5")
-			return true
-		end
-		if profile:hasBuffSelf("tenshijin") and TimeSince(profile.ntsj4) > 10000 and profile.checkEach({"tsj4"}) then
-			profile.ntsj4 = Now()
-			d("tsj4")
-			return true
-		end		
-
-		--casting ninjutsu any
-		if profile.castingNinjutsu then
-			d(profile.mudracounter < profile.mudranumber)
-			if profile.mudracounter < profile.mudranumber then
-				if profile.checkEach({profile.mudra[profile.mudracounter+1]}) then
-					profile.mudracounter = profile.mudracounter + 1
-					d(profile.mudracounter)
-					return true
-				end
-			else
-				if profile.checkEach({profile.mudraresult}) then
-					profile.castingNinjutsu = false
-					return true
-				end			
+		-- aoe tsj3 into tsj4 (katon) into tsj8 (doton)
+		-- single tsj 1 into tsj5 (raiton) into tsj9 (suiton)
+		if profile.counttarget() > 2 then
+			if profile:hasBuffSelf("tenshijin") and profile.waitedOGCD(1500) and profile.ntsj3 and profile.checkEach({"tsj3"}) then
+				profile.ntsj3 = false
+				profile.ogcdtimer = Now()
+				return true
+			end
+			if profile:hasBuffSelf("tenshijin") and profile.waitedOGCD(1500) and profile.ntsj4 and profile.checkEach({"tsj4"}) then
+				profile.ntsj4 = false
+				profile.ogcdtimer = Now()
+				return true
+			end		
+			if profile:hasBuffSelf("tenshijin") and profile.waitedOGCD(1500) and profile.ntsj8 and profile.checkEach({"tsj8"},"player")  then
+				profile.ntsj8 = false
+				profile.ogcdtimer = Now()
+				return true
+			end		
+		else
+			if profile:hasBuffSelf("tenshijin") and profile.waitedOGCD(1500) and profile.ntsj1 and profile.checkEach({"tsj1"}) then
+				profile.ntsj1 = false
+				profile.ogcdtimer = Now()
+				return true
+			end
+			if profile:hasBuffSelf("tenshijin") and profile.waitedOGCD(1500) and profile.ntsj5 and profile.checkEach({"tsj5"}) then
+				profile.ntsj5 = false
+				profile.ogcdtimer = Now()
+				return true
+			end		
+			if profile:hasBuffSelf("tenshijin") and profile.waitedOGCD(1500) and profile.ntsj9 and profile.checkEach({"tsj9"})  then
+				profile.ntsj9 = false
+				profile.ogcdtimer = Now()
+				return true
 			end
 		end
+		if profile:hasBuffSelf("tenshijin") then return false end
+		
+		--casting ninjutsu any
+		if profile.castingNinjutsu then
+			--d(profile.mudracounter < profile.mudranumber)
+			if profile.waitedOGCD(500) and profile.mudracounter < profile.mudranumber then
+				profile.ogcdtimer = Now()
+				if  profile.checkEach({profile.mudra[profile.mudracounter+1]}) then
+					profile.mudracounter = profile.mudracounter + 1
+					--d(profile.mudracounter)
+					return true
+				end
+			elseif profile.waitedOGCD(500) and profile.mudracounter >= profile.mudranumber then
+				profile.ogcdtimer = Now()
+				if profile.mudratarget == "player" then
+					if profile.checkEach({profile.mudraresult},"player") then
+						profile.castingNinjutsu = false
+						return true
+					end				
+				else
+					if profile.checkEach({profile.mudraresult}) then
+						profile.castingNinjutsu = false
+						return true
+					end				
+				end
+			end
+		end
+		if profile.castingNinjutsu then return false end
 		--kassatsu
-		if profile.checkEach({"kassatsu"},"player") and (not profile["ten"].isoncd) then
+		if currentTarget.distance < 5 and not Player:IsMoving() and (not profile["ten"].isoncd) and profile.checkEach({"kassatsu"},"player") then
 			return true
 		end
 		--hyoton (kassatsu)
-		if profile:hasBuffSelf("kassatsu") and profile.checkEach({"hyoton2"}) then
-			return true
-		end
+		-- if profile:hasBuffSelf("kassatsu") and profile.checkEach({"hyoton2"}) then
+			-- return true
+		-- end
 		--calling hyoshoranryu
-		if profile:hasBuffSelf("kassatsu") and (not profile["ten"].isoncd) then
-			--d(profile["meisui"].isoncd)
-			profile.mudracounter = 0
-			profile.mudranumber = 2
-			profile.mudra = {}
-			profile.mudra = {"chi","jin2"}
-			profile.mudraresult = "hyoton2"
-			profile.castingNinjutsu = true
-			--d("preparing to use meisui")
-		end		
+		--if profile:hasBuffSelf("kassatsu") and (not profile["ten"].isoncd) then
+		if Player.level >= 76 then
+			if profile.counttarget() > 2 then
+				if profile:hasBuffSelf("kassatsu") then
+					--d(profile["meisui"].isoncd)
+					profile.mudracounter = 0
+					profile.mudranumber = 2
+					--profile.mudra = {}
+					profile.mudra = {"chi","ten2"}
+					profile.mudraresult = "katon2"
+					profile.ninjutsumaxtimer = Now()
+					profile.castingNinjutsu = true
+					profile.mudratarget = "target"
+					return false
+				end		
+			else
+				if profile:hasBuffSelf("kassatsu") then
+					--d(profile["meisui"].isoncd)
+					profile.mudracounter = 0
+					profile.mudranumber = 2
+					--profile.mudra = {}
+					profile.mudra = {"chi","jin2"}
+					profile.mudraresult = "hyoton2"
+					profile.ninjutsumaxtimer = Now()
+					profile.castingNinjutsu = true
+					profile.mudratarget = "target"
+					return false
+				end	
+			end
+		else
+			if profile.counttarget() > 2 then
+				if profile:hasBuffSelf("kassatsu") then
+					--d(profile["meisui"].isoncd)
+					profile.mudracounter = 0
+					profile.mudranumber = 2
+					--profile.mudra = {}
+					profile.mudra = {"chi","ten2"}
+					profile.mudraresult = "katon"
+					profile.ninjutsumaxtimer = Now()
+					profile.castingNinjutsu = true
+					profile.mudratarget = "target"
+					return false
+				end		
+			else
+				if profile:hasBuffSelf("kassatsu") then
+					--d(profile["meisui"].isoncd)
+					profile.mudracounter = 0
+					profile.mudranumber = 2
+					--profile.mudra = {}
+					profile.mudra = {"ten","chi2"}
+					profile.mudraresult = "raiton"
+					profile.ninjutsumaxtimer = Now()
+					profile.castingNinjutsu = true
+					profile.mudratarget = "target"
+					return false
+				end	
+			end		
+		end
 		--meisui
 		if profile.checkEach({"meisui"},"player") then
 			return true
 		end
 		--calling suiton (meisui)
-		if (not profile["meisui"].isoncd) and (not profile.castingNinjutsu) and Player.gauge ~= nil and Player.gauge[1] <= 50  and (not profile["ten"].isoncd) then
+		if currentTarget.distance < 5 and  Player.level >= 72 and (not profile["meisui"].isoncd) and (not profile.castingNinjutsu) and Player.gauge ~= nil and Player.gauge[1] <= 50  and (not profile["ten"].isoncd) then
 			--d(profile["meisui"].isoncd)
 			profile.mudracounter = 0
 			profile.mudranumber = 3
 			profile.mudra = {"chi","ten2","jin2"}
 			profile.mudraresult = "suiton"
+			profile.ninjutsumaxtimer = Now()
 			profile.castingNinjutsu = true
+			profile.mudratarget = "target"
+			profile.ogcdtimer = Now()
+			return false
 			--d("preparing to use meisui")
 		end
 		--trickattack
@@ -278,29 +358,96 @@ function profile.Cast()
 			return true
 		end
 		--calling suiton (trickattack)
-		if (not profile["trickattack"].isoncd) and (not profile.castingNinjutsu) and (not profile["ten"].isoncd) then
+		if currentTarget.distance < 5 and (not profile["trickattack"].isoncd) and (not profile.castingNinjutsu) and (not profile["ten"].isoncd) then
 			profile.mudracounter = 0
 			profile.mudranumber = 3
 			profile.mudra = {"chi","ten2","jin2"}
 			profile.mudraresult = "suiton"
+			profile.ninjutsumaxtimer = Now()
+			profile.mudratarget = "target"
 			profile.castingNinjutsu = true
+			profile.ogcdtimer = Now()
+			return false
 		end
 		--raiton
-		if profile.checkEach({"raiton"}) then
-			return true
-		end
-		--calling raiton 
-		if (profile["trickattack"].isoncd) and (profile["meisui"].isoncd)  and (not profile.castingNinjutsu) and (not profile["ten"].isoncd)  then
-			profile.mudracounter = 0
-			profile.mudranumber = 2
-			profile.mudra = {"ten","chi2"}
-			profile.mudraresult = "raiton"
-			profile.castingNinjutsu = true
-		end		
+		-- if profile.checkEach({"raiton"}) then
+			-- return true
+		-- end
 		--huraijin
-		if Player.gauge ~= nil and Player.gauge[2] == 0 and profile.checkEach({"huraijin"}) then
-			return true
+		if Player.level >= 60 then
+			if Player.gauge ~= nil and Player.gauge[2] == 0 and profile.checkEach({"huraijin"}) then
+				return true
+			end
+		else
+			-- if Player.gauge ~= nil and Player.gauge[2] == 0 and profile.checkEach({"huton"},"player") then
+				-- return true
+			-- end		
+			if Player.gauge ~= nil and Player.gauge[2] == 0 and (not profile.castingNinjutsu) and (not profile["ten"].isoncd)  then
+				profile.mudracounter = 0
+				profile.mudranumber = 3
+				profile.mudra = {"jin","chi2","ten2"}
+				profile.mudraresult = "huton"
+				profile.ninjutsumaxtimer = Now()
+				profile.mudratarget = "player"
+				profile.castingNinjutsu = true
+				profile.ogcdtimer = Now()
+				return false				
+			end
 		end		
+		--calling raiton
+		if Player.level >= 72 then
+			if profile.counttarget() > 2 then
+				if currentTarget.distance < 5 and (profile["trickattack"].isoncd) and (profile["meisui"].isoncd)  and (not profile.castingNinjutsu) and (not profile["ten"].isoncd)  then
+					profile.mudracounter = 0
+					profile.mudranumber = 2
+					profile.mudra = {"chi","ten2"}
+					profile.mudraresult = "katon"
+					profile.ninjutsumaxtimer = Now()
+					profile.mudratarget = "target"
+					profile.castingNinjutsu = true
+					profile.ogcdtimer = Now()
+					return false			
+				end		
+			else
+				if currentTarget.distance < 5 and (profile["trickattack"].isoncd) and (profile["meisui"].isoncd)  and (not profile.castingNinjutsu) and (not profile["ten"].isoncd)  then
+					profile.mudracounter = 0
+					profile.mudranumber = 2
+					profile.mudra = {"ten","chi2"}
+					profile.mudraresult = "raiton"
+					profile.ninjutsumaxtimer = Now()
+					profile.mudratarget = "target"
+					profile.castingNinjutsu = true
+					profile.ogcdtimer = Now()
+					return false			
+				end
+			end
+		else
+			if profile.counttarget() > 2 then
+				if currentTarget.distance < 5 and (profile["trickattack"].isoncd) and (not profile.castingNinjutsu) and (not profile["ten"].isoncd)  then
+					profile.mudracounter = 0
+					profile.mudranumber = 2
+					profile.mudra = {"chi","ten2"}
+					profile.mudraresult = "katon"
+					profile.ninjutsumaxtimer = Now()
+					profile.mudratarget = "target"
+					profile.castingNinjutsu = true
+					profile.ogcdtimer = Now()
+					return false			
+				end		
+			else
+				if currentTarget.distance < 5 and (profile["trickattack"].isoncd) and (not profile.castingNinjutsu) and (not profile["ten"].isoncd)  then
+					profile.mudracounter = 0
+					profile.mudranumber = 2
+					profile.mudra = {"ten","chi2"}
+					profile.mudraresult = "raiton"
+					profile.ninjutsumaxtimer = Now()
+					profile.mudratarget = "target"
+					profile.castingNinjutsu = true
+					profile.ogcdtimer = Now()
+					return false			
+				end
+			end		
+		end
 		--ogcdtimer
 		if profile.checkEach({"dreamwithinadream"}) then
 			return true
@@ -312,7 +459,7 @@ function profile.Cast()
 		if Player.gauge ~= nil and Player.gauge[1] >= 50 and profile.checkEach({"bunshin"},"player") then
 			return true
 		end		
-		if profile.counttarget() > 2 then
+		if (profile.counttarget() > 2) or (Player.level < 68) then
 			if Player.gauge ~= nil and Player.gauge[1] >= 50 and profile.checkEach({"hellfrogmedium"}) then
 				return true
 			end		
